0b69a4bec4165804545c033336a3a22d16a30456
Eva Szin Takacs <szin.takacs.eva@gmail.com>
2019-02-04 16:22:20 +0100

    Add initial version

---
+++ R/mlhelper.R
@@ -0,0 +1,8 @@
+#' mlhelper: Helper functions for EDA and ml
+#'
+#' Collection of EDA and helper df preprocess functions
+#'
+#' @docType package
+#' @name mlhelper
+#' @author Eva Szin Takacs, \email{szin.takacs.eva@gmail.com}
+"_PACKAGE"
---
+++ R/weather.R
@@ -0,0 +1,12 @@
+#' Random Weather Data
+#'
+#' A dataset containing randomly generated weather data.
+#'
+#' @format A data frame of 7 rows and 3 columns
+#' \describe{
+#'  \item{Day}{Numeric values giving day of the week, 1 = Monday, 7 = Sunday}
+#'  \item{Temp}{Numeric values giving temperature in degrees Celsius}
+#'  \item{Weather}{Character values describing the weather on that day}
+#' }
+#' @source Randomly generated data
+"weather"
